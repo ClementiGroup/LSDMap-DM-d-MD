2a2b7c819daa57b87eeb3bd03c256f0606194b10
ajkluber <alexkluber@gmail.com>
2015-04-13 18:09:46 -0500

    Tested get_idxs_thread with Scatterv. Works.

--- lsdmap/mpi/p_index.py
+++ lsdmap/mpi/p_index.py
@@ -1,31 +1,8 @@
 import numpy as np
+from mpi4py import MPI
+
 
 def get_idxs_thread(comm, npoints):
-    """
-    get the indices of the coordinates that will be consider by each CPU
-    """
-    size = comm.Get_size()
-    rank = comm.Get_rank()
-
-    npoints_per_thread = np.array([npoints/size for idx in xrange(size)])
-
-    for idx in range(npoints%size):
-        npoints_per_thread[idx] += 1
-
-    if rank == 0:
-        idx_shift = 0
-        idxs_threads = []
-        for idx in npoints_per_thread:
-            idxs_threads.append(range(idx_shift,idx_shift+idx))
-            idx_shift += idx
-    else:
-        idxs_threads = None
-
-    idxs_thread = comm.scatter(idxs_threads, root=0)
-
-    return idxs_thread
-
-def get_idxs_thread_v(comm, npoints):
     """ Get indices for processor using Scatterv
 
     Note: 
